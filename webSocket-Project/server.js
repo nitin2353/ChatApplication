3fa3000c0c497864107df594dc9e9832132aa5b4
nitin2353 <154704828+nitin2353@users.noreply.github.com>
2025-03-27 15:56:14 +0530

    Add files via upload

---
+++ webSocket-Project/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const {Server} = require('socket.io');
+const ejs = require('ejs');
+const app = express();
+const port = 4000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = http.createServer(app);
+
+const io = new Server(server);
+var newUid = " ";
+
+io.on('connection', (socket) => {
+    socket.on("new-message", (message) => {
+        io.emit('user-message', message, newUid);
+        // io.emit('user-message', message, socket.id);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/login', (req, res) => {
+    newUid = req.query.name
+    res.render('index.ejs');
+})
+
+server.listen(port, () => {
+    console.log('Server is started at port', port);
+})
